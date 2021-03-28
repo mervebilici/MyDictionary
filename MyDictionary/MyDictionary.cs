3aa239ee2e72420d404d64e75c99583d8a71edf6
Merve Bilici <60577210+mervebilici@users.noreply.github.com>
2021-03-28 15:06:03 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<X, Y>
+    {
+        X[] Key;
+        Y[] Volue;
+
+        public MyDictionary() //contructor oluşturduk
+        {
+            Key = new X[0];
+            Volue = new Y[0];
+        }
+        public void Add(X key, Y volue)
+        {
+            X[] tempKey = Key;
+            Y[] tempVolue = Volue;
+
+            Key = new X[Key.Length + 1];
+            Volue = new Y[Volue.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                Key[i] = tempKey[i];
+            }
+            for (int i = 0; i < tempVolue.Length; i++)
+            {
+                Volue[i] = tempVolue[i];
+            }
+            key = Key[Key.Length - 1];
+            volue = Volue[Volue.Length - 1];
+        }
+
+        public int count
+        {
+            get { return Key.Length; }
+
+        }
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, int> myDictionary = new MyDictionary<string, int>();
+            myDictionary.Add("Ali", 001);
+            myDictionary.Add("Ayşe", 002);
+            myDictionary.Add("Mert", 003);
+            myDictionary.Add("Selin", 004);
+            myDictionary.Add("Ecem", 005);
+
+            Console.WriteLine(myDictionary.count);
+        }
+    }
+}
